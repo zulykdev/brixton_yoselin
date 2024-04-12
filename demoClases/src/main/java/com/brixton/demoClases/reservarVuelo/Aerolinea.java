86d9b809e73679bd393130a5bc87459610e2195b
zulykdev <166577302+zulykdev@users.noreply.github.com>
2024-04-11 22:10:49 -0500

    Actualizacion

--- demoClases/src/main/java/com/brixton/demoClases/reservarVuelo/Aerolinea.java
+++ demoClases/src/main/java/com/brixton/demoClases/reservarVuelo/Aerolinea.java
@@ -10,9 +10,22 @@ public class Aerolinea {
     private String nombreEmpresa;
     private List<String> lugareDeDestino;
     private Vuelo fechaDisponible;
+    private Map<Politica> politicasVuelo;
 
-    public double ofertaDescuento(List<String> lugareDeDestino){
+    public boolean isValidDestination(String destino) {
+        //Destino que ingrese debe buscarse o compararse con la lista de Destinos Disponibles de la aerolinea
+        //Si es un destino valido, retornas true, isValidDestination
+        //Si es un destino invalido, retornas false, isValidDestination 
+        
+    }
 
+    public boolean isAvailableSit(String destino, LocalDateTime fechaIda, LocalDateTime fechaRetorno) {
+        //busca en los vuelos existentes de esa fecha y revisa los asientos. 
+    }
+
+    
+
+    public Oferta ofertaDescuento(List<String> lugareDeDestino){
         return 0.0;
     }
 }
