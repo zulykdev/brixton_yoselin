25f1f43a57c36da24b87b0532ddbf22b82a619d8
Yoselin <yrudasb@gmail.com>
2024-05-17 22:32:45 -0500

    Correcion de la excepcion

--- demoClases/src/main/java/com/brixton/demoClases/exception/SumaIterativa.java
+++ demoClases/src/main/java/com/brixton/demoClases/exception/SumaIterativa.java
@@ -8,26 +8,38 @@ public class SumaIterativa {
 
         Scanner input = new Scanner(System.in);
 
-        String continuar = "y";
-        while (continuar.equals("y")) {
-            try {
-                System.out.println("Ingrese el primer valor: ");
-                double num1 = input.nextDouble();
+       String continuar = "y";
+        //CONSTANTE:valor que no cambia
+        String CONTINUAR = "Y";
 
-                System.out.println("Ingrese el segundo valor: ");
-                double num2 = input.nextDouble();
+        //while (continuar.equals("y")) {
+            while (CONTINUAR.equals(continuar.toUpperCase())) {
+                try {
+                    System.out.println("Ingrese el primer valor: ");
+                    double num1 = input.nextDouble();
 
-                double suma = num1 + num2;
+                    System.out.println("Ingrese el segundo valor: ");
+                    double num2 = input.nextDouble();
 
-                System.out.println("El resultado de la suma es : " + suma);
-            } catch (Exception e) {
-                System.out.println("Solo se permite valor numérico: " + e.getMessage());
+                    double suma = num1 + num2;
+
+                    System.out.println("El resultado de la suma es : " + suma);
+                } catch (Exception e) {
+                    System.out.println("Solo se permite valor numérico: " + e.getMessage());
+                }
+
+                System.out.println("Desea continuar? : y/n");
+                continuar = input.next();
+                while (!continuar.equalsIgnoreCase("y") && !continuar.equalsIgnoreCase("n")) {
+                    System.out.println("Entrada inválida. Por favor ingrese 'y' para continuar o 'n' para salir: ");
+                    continuar = input.next();
+
+
+                }
             }
-
-            System.out.println("Desea continuar? : y/n");
-            continuar = input.next();
-
         }
     }
-}
+
+
+
 
